4cfcdd15ab420beb248b874b0ac45054b134b84e
Dragon Dave McKee <dave.mckee@gmail.com>
2022-10-16 13:46:48 +0100

    add score

--- exoplanet.py
+++ exoplanet.py
@@ -1,3 +1,4 @@
+from locale import ALT_DIGITS
 import requests
 import lxml.html
 import re
@@ -15,6 +16,9 @@ LAT = 54.59769273166332 # -90..90
 NOW = datetime.date.today()
 TODAY = NOW.isoformat()
 MONTH = (NOW + datetime.timedelta(days=30)).isoformat()
+DUSK_TIME = datetime.time(hour=19, minute=30)
+END_CUTOFF_HOUR = 3
+ALT_MIN = 30
 
 CANNED = True
 if CANNED:
@@ -44,6 +48,65 @@ def xpath(root, x):
     else:
         return None
 
+def score(data):
+    min_altitude = min(data['begin_ele'], data['mid_ele'], data['end_ele'])
+    if min_altitude < ALT_MIN:
+        alt_score = 0
+    else:
+        alt_score = 1 - (90-min_altitude)/(90-ALT_MIN)
+    print (f"min alt {min_altitude}, score {alt_score}")
+    
+    if data['delta-mag'] > 0.005:
+        dmag_score = data['delta-mag'] * 33
+    else:
+        dmag_score = 0
+    print (f"dmag {data['delta-mag']}, score {dmag_score}")
+
+    # dur_score = min(1, 1/(data['duration'] / 60))
+    # print (f"duration {data['duration']}, score {dur_score}")
+    
+    begin_time = datetime.datetime.strptime(data['begin_time'],  "%H:%M")
+    dusk_time = datetime.datetime(year=1900, month=1, day=1, hour=DUSK_TIME.hour, minute=DUSK_TIME.minute)
+    if begin_time.hour < 12:
+        dusk_score = 1
+        dusk_delay = "early morning"
+    else:
+        dusk_delay = begin_time - dusk_time
+        dusk_hours = dusk_delay.days*24 + (dusk_delay.seconds/60/60)
+        if dusk_hours < 1:
+            dusk_score = max(0, dusk_hours)
+        else:
+            dusk_score = 1
+    print (f"start {data['begin_time']}, dusk-delay {dusk_delay}, dusk-score {dusk_score}")
+  
+    end_time = datetime.datetime.strptime(data['end_time'], "%H:%M").time()
+    print(end_time)
+    if end_time.hour > 12:
+        end_score = 1
+    elif end_time.hour >= END_CUTOFF_HOUR:
+        end_score = 0
+    else:
+        end_score = 1 - (end_time.hour + (end_time.minute / 60)) / END_CUTOFF_HOUR
+    print (f"end time {end_time}, score {end_score}")
+
+    # actual mag
+    mag = int(data['mag'])
+    if mag > 14:
+        mag_score = 0
+    elif mag < 10:
+        mag_score = 1
+    else:
+        mag_score = 1 - (mag-10)/4
+    print (f"magnitude {mag} score {mag_score}")
+
+    # moon
+    print (end_score, dusk_score, dmag_score, alt_score, mag_score)
+    score = end_score * dusk_score * dmag_score * alt_score * mag_score
+    print(score)
+    data['score'] = f"{score:.3f}"
+
+    print()
+
 def parse_transit(html):
     if "predict_detail" not in html:
         return
@@ -69,8 +132,7 @@ def parse_transit(html):
 
     data['begin_time'], data['mid_time'], data['end_time'] = begin_time, mid_time, end_time
     data['begin_ele'], data['begin_dir'], data['mid_ele'], data['mid_dir'], data['end_ele'], data['end_dir'] = begin_ele, begin_dir, mid_ele, mid_dir, end_ele, end_dir
-
-    data['duration'], data['mag'], data['delta-mag'] = tds[3:]
+    data['duration'], data['mag'], data['delta-mag'] = map(float, tds[3:])
     span = re.search(r"<span style='font-size: 80%'>(.*?)</span>", html).group(1)
     data['elems'], data['ra'], data['dec'] = re.search('(.*)<br/>RA: (.*)<br/>DE: (.*)', span).groups()
     return data
@@ -84,10 +146,10 @@ def format_transit(data):
         date = date - datetime.timedelta(days=1)
     data['date'] = datetime.datetime.strftime(date, "%d %b")
     
-    data['mid_ele'] = data['mid_ele'].replace('\xb0', '')
+    data['mid_ele'] = int(data['mid_ele'].replace('\xb0', ''))
     
-    data['begin_ele'] = data['begin_ele'].replace('\xb0', '')
-    data['end_ele'] = data['end_ele'].replace('\xb0', '')
+    data['begin_ele'] = int(data['begin_ele'].replace('\xb0', ''))
+    data['end_ele'] = int(data['end_ele'].replace('\xb0', ''))
     return data
 
 with open("exoplanets.csv", "w") as outfile:
@@ -95,9 +157,10 @@ with open("exoplanets.csv", "w") as outfile:
     first = True
     for t in transits:
         data = parse_transit(t)
-        if data and first:
-            csvwriter.writerow(dict(data))
-            first = False
         if data:
             data = format_transit(data)
+            score(data)
+            if first:
+                csvwriter.writerow(dict(data))
+                first = False
             csvwriter.writerow(dict(data).values())
